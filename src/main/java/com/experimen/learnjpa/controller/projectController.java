bec765175abcd5f6e08a3f3b2f550c3123b544f8
Wardani <dediduank@gmail.com>
2021-12-13 16:50:03 +0700

    upload to github

--- src/main/java/com/experimen/learnjpa/controller/projectController.java
+++ src/main/java/com/experimen/learnjpa/controller/projectController.java
@@ -6,6 +6,7 @@ import com.experimen.learnjpa.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/check/student")
 public class projectController {
     
